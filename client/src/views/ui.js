05724edfc6448d0eef32e76b767c55f6220b4fc4
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-07 15:05:00 +0100

    weather widget added

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -2,14 +2,33 @@ var WeatherApi = require( '../models/weatherApi' );
 var NewsFeed = require( '../models/newsFeed' );
 
 var UI = function() {
-  var newsFeed = new NewsFeed();
+  var body = document.querySelector('body')
+  var container = document.createElement('div');
+  container.setAttribute("id", "container")
+  body.appendChild(container)
+
   var weather = new WeatherApi();
+  var newsFeed = new NewsFeed();
   newsFeed.all( this.newsRender );
-  weather.get( this.render );
+  weather.get( this.weatherRender );
 }
 
-UI.prototype.render = function( thing ) {
-  console.log( thing );
+UI.prototype.weatherRender = function( weather ) {
+  var div = document.createElement("div");
+  div.setAttribute("id", "weather-widget");
+
+  var icon = document.createElement("img");
+  icon.src = "http://openweathermap.org/img/w/"+ weather.icon +".png";
+  div.appendChild(icon);
+
+  var description = document.createElement("p");
+  description.innerText = weather.description;
+  div.appendChild(description)
+
+  var container = document.querySelector("#container");
+  container.appendChild( div )
+
+  console.log( weather);
 }
 
 UI.prototype.newsRender = function( news ) {
