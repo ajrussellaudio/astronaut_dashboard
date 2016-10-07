42132e90bbcace8d3f0146aa6240683a6e96e7be
Alan Russell <ajrussellaudio@gmail.com>
2016-10-07 11:58:20 +0100

    add weather model

---
+++ client/src/models/weather.js
@@ -0,0 +1,8 @@
+var Weather = function( options ) {
+  this.id = options.id;
+  this.main = options.main;
+  this.description = options.description;
+  this.icon = options.description;
+}
+
+module.exports = Weather;
