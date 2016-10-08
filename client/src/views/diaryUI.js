415e506e0c466fe2a51059f910143977f54a49e1
Hannah Stewart <hmstewart84@gmail.com>
2016-10-08 14:06:18 +0100

    make diary UI constructor

--- client/src/views/diaryUI.js
+++ client/src/views/diaryUI.js
@@ -1,6 +1,13 @@
-var DiaryEntry = require("../models/diaryEntry")
+var DiaryFeed = require("../models/diaryFeed")
 
 
-var DiaryFeedUI = function(){
-
+var DiaryFeed = function(){
+var diaryFeed = new DiaryFeed();
+diaryFeed.all(this.render);
 };
+
+DiaryFeed.prototype.render = function( diary ) {
+
+}
+
+
