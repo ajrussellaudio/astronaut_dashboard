06c1de37474f73223935e3d99ce89641d1e1fc4f
davidmorrison1981 <david.morrison@geminises.com>
2016-10-08 14:24:40 +0100

    text box on page plus submit button works

--- client/src/views/diaryFormUI.js
+++ client/src/views/diaryFormUI.js
@@ -4,16 +4,27 @@ var DiaryEntryUI = function() {
   var titleInput = document.createElement( "input" );
   titleInput.type = "text";
   titleInput.name = "title";
+  // input type date is a thing
+
+  var submit = document.createElement('input');
+  submit.type = 'button'
+  submit.value = "Submit";
+  submit.onclick = function(){
+    console.log('we win mutha fuckker')
+  }
 
   // var submit = submit button;
   // onclick = function() { new DiaryEntry( all the stuff above ) }
   // take that new thing and Stringify/POST
-
+ 
 
   //THEN
-  // var form = document.createElement( "form" );
+  var form = document.createElement( "form" );
 
-  // form.appendChild( titleInput );
+  form.appendChild( titleInput );
+  form.appendChild( submit);
+  var divDiary = document.querySelector("#diary");
+  divDiary.appendChild(form);
 }
 
 module.exports = DiaryEntryUI;
