7d29f619de43b16e99abcb40c64bcba0987b72df
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-11 14:20:27 +0100

    HeaderUI developed to include name functions

--- client/src/views/headerUI.js
+++ client/src/views/headerUI.js
@@ -5,6 +5,7 @@ var HeaderUI = function(){
 
   this.headerContainer = document.querySelector("#header")
   this.renderName(headerContainer);
+
   this.renderClock(headerContainer);
   this.renderWeather(headerContainer);
 }
@@ -12,21 +13,31 @@ var HeaderUI = function(){
 
 
 
-HeaderUI.prototype.renderName = function(container){
+HeaderUI.prototype.renderName = function(){
   //make name div
   var nameDiv = document.createElement("div");
   nameDiv.setAttribute("id", "name-container");
 
-
-  if (name) {
-    renderNameElement(nameDiv);
+  if (this.asstronautInfo.name){
+    makeNameElement(nameDiv);
   } else {
-    renderNameQuestion(nameDiv);
+    makeNameBox(nameDiv);
   }
+  this.headerContainer.appendChild(nameDiv);
 }
 
-HeaderUI.prototype.renderNameElemet = function(contianer)
-HeaderUI.prototype
+HeaderUI.prototype.renderNameElement = function(contianer){
+  var h1 = document.createElement("h1");
+  h1.setAttribute()
+  h1.innerText = this.asstronautInfo.name;
+  container.appendChild(h1);
+}
+
+HeaderUI.prototype.renderNameBox = function(container){
+  var nameInput = document.createElement("textarea");
+  nameInput.setAttribute("id","name-input");
+
+}
 
 
 
