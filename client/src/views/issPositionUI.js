3344e0e272f8e0771173f69f92f401d0c4819a9b
Michael Snaddon <msnaddon0991@gmail.com>
2016-10-12 12:59:47 +0100

    styled widget

--- client/src/views/issPositionUI.js
+++ client/src/views/issPositionUI.js
@@ -8,17 +8,31 @@ var MapUI = function(){
 
 MapUI.prototype.render = function( map ) { 
 
-  var div = document.createElement('div');
-  div.setAttribute('id', 'weather');
-  div.setAttribute("class","dashboard-widget")
-  var widget = document.querySelector("#widgets");
-  widget.appendChild(div);
-  var coords = {lat: 0, lng:0}
-  var map = new Map(div, coords, 5);
-  var issPosition = new IssPosition();
-  
-  setInterval(function(){
+  var widgets = document.querySelector("#widgets");
 
+  var div = document.createElement('div');
+  div.setAttribute('id', 'iss-pos');
+  div.setAttribute("class","dashboard-widget");
+
+  divHeader = document.createElement('p');
+  divHeader.setAttribute("id","iss-pos-header")
+  divHeader.innerText = "Current Position"
+
+  mapDiv = document.createElement('div');
+  mapDiv.setAttribute("id","iss-pos-map");
+
+  div.appendChild(divHeader)
+  div.appendChild(mapDiv)
+
+
+  widgets.appendChild(div);
+
+
+  // setting up map and listener
+  var coords = {lat: 0, lng:0};
+  var map = new Map(mapDiv, coords, 5);
+  var issPosition = new IssPosition();
+  setInterval(function(){
     issPosition.get(function(issPosition){
       console.log ("Pinging iss", issPosition)
       map.addMarker(issPosition);
